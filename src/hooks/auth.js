028892bfd32c30e03242916417fb4ecbc9aa22d6
jdunk <jdunk@users.noreply.github.com>
2021-05-30 17:47:11 -0400

    cjs vs mjs related changes

--- src/hooks/auth.js
+++ src/hooks/auth.js
@@ -1,5 +1,5 @@
 import { useState, useEffect, createContext, useContext } from 'react';
-import apiClient from '../utils/client/api-client';
+import apiClient from '../utils/client/api-client.js';
 
 export const AuthContext = createContext();
 
